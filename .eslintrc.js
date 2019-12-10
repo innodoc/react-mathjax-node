60b569be40474fd02437cf451cb65b977af12500
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-12-10 13:26:37 +0100

    Update project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,17 +17,17 @@ module.exports = {
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
-    'jsx-a11y/anchor-is-valid': ['off'],
+    'jsx-a11y/anchor-is-valid': 'off',
     'react/jsx-filename-extension': [
       'error',
       {
         extensions: ['.js'],
       },
     ],
-    'react/jsx-one-expression-per-line': ['off'],
-    'react/forbid-prop-types': ['off'],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/forbid-prop-types': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'prettier/prettier': ['error'],
+    'prettier/prettier': 'error',
   },
 }
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,16 @@
-module.exports = {
-  presets: [['@babel/env', { modules: false }], '@babel/react'],
-  plugins: ['@babel/plugin-syntax-dynamic-import'],
-  env: {
-    cjs: {
-      presets: ['@babel/env', '@babel/react'],
+module.exports = (api) => {
+  const config = {
+    presets: [['@babel/env', { modules: false }], '@babel/react'],
+    plugins: ['@babel/plugin-syntax-dynamic-import'],
+    env: {
+      cjs: {
+        presets: ['@babel/env', '@babel/react'],
+      },
     },
-  },
+  }
+  if (api.env('test')) {
+    config.presets[0][1].modules = 'auto'
+    config.presets[0][1].targets = { node: 'current' }
+  }
+  return config
 }
