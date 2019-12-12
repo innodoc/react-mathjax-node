838e9f77b1e86b81653c8d601c20e0275a7fd371
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-12-12 16:13:02 +0100

    Add useMathJax.test.js (#1)

--- test/useMathJax.test.js
+++ test/useMathJax.test.js
@@ -1,34 +1,86 @@
-import React, { useEffect, useContext } from 'react'
+import React from 'react'
 import { mount } from 'enzyme'
 
-import { Provider, Context, useMathJax } from '..'
+import useMathJax from '../src/useMathJax'
 
-const MathJaxComponent = ({ callback, mathType, texCode }) => {
-  const { addCallback, removeCallback } = useContext(Context)
-  useEffect(() => {
-    addCallback(callback)
-    return () => removeCallback(callback)
-  })
+let mockPromiseMakers
+let mockRef
+let mockProcessPromises
+let mockUseEffectFunc
+jest.mock('react', () => {
+  const ActualReact = jest.requireActual('react')
+  return {
+    ...ActualReact,
+    useContext: () => {
+      mockPromiseMakers = ActualReact.useRef([])
+      return { promiseMakers: mockPromiseMakers }
+    },
+    useEffect: (func) => {
+      mockUseEffectFunc = func
+    },
+    useRef: () => {
+      mockRef = ActualReact.useRef()
+      return mockRef
+    },
+  }
+})
+
+const MathJaxComponent = ({ mathType, texCode }) => {
   const elem = useMathJax(texCode, mathType)
   return <div ref={elem} />
 }
 
+const texCode = 'f(x)=x^2'
+
 describe('useMathJax', () => {
-  it('should render', (done) => {
-    const texCode = 'f(x)=x^2'
-    let wrapper
-    const callback = () => {
-      expect(wrapper.html()).toBe(`<div><span>${texCode}</span></div>`)
-      done()
+  it.each(['inline', 'display'])(
+    'should call MathJax and manage nodes (%s)',
+    (mathType, done) => {
+      let cleanupFunc
+
+      window.MathJax = {
+        tex2chtmlPromise: (_texCode, options) => {
+          expect(_texCode).toBe(texCode)
+          expect(options).toEqual({
+            mock: 'metrics',
+            display: mathType === 'display',
+          })
+          return new Promise((resolve) => {
+            const el = window.document.createElement('div')
+            el.id = 'mockid'
+            resolve(el)
+          })
+        },
+        getMetricsFor: (elem, display) => {
+          expect(elem).toBe(mockRef.current)
+          expect(display).toBe(mathType === 'display')
+          return { mock: 'metrics' }
+        },
+      }
+
+      mockProcessPromises = () => {
+        expect(mockPromiseMakers.current).toHaveLength(1)
+        mockPromiseMakers.current[0]().then(() => {
+          // check mathJaxNodes were appended
+          expect(mockRef.current.children).toHaveLength(1)
+          expect(mockRef.current.children[0].id).toBe('mockid')
+          // check removal of mathJaxNodes on cleanup
+          cleanupFunc()
+          expect(mockRef.current.children).toHaveLength(0)
+          done()
+        })
+      }
+
+      const wrapper = mount(
+        <MathJaxComponent mathType={mathType} texCode={texCode} />
+      )
+
+      // Call useEffect *after* Component is mounted, so ref is set
+      cleanupFunc = mockUseEffectFunc()
+      mockProcessPromises()
+
+      const div = wrapper.childAt(0)
+      expect(div.instance()).toBe(mockRef.current)
     }
-    wrapper = mount(
-      <Provider>
-        <MathJaxComponent
-          callback={callback}
-          mathType="inline"
-          texCode={texCode}
-        />
-      </Provider>
-    )
-  })
+  )
 })
